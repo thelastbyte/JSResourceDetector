bc273f6ebf77526141a58857c1d8be9c817e0022
thelastbyte <sanjay.parab@gmail.com>
2015-08-27 00:26:32 -0700

    Update jstest.js

--- jstest.js
+++ jstest.js
@@ -1,5 +1,8 @@
     alert("Start");
     var txt = document.scripts.length;
     alert("No of scripts loaded " + document.scripts.length );
-    alert("Code = " + document.scripts[0].text);
+    alert("Code 0= " + document.scripts[0].text);
+    alert("Code 1= " + document.scripts[1].text);
+    alert("Code 2= " + document.scripts[2].text);
+    alert(document.domain);
     alert('end');
